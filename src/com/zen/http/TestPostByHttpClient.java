bb58d3303c383520e1247c90805b2a36b6b9826d
zentalent <zentalent@163.com>
2016-08-25 14:01:03 +0800

    删除无用包引用

--- src/com/zen/http/TestPostByHttpClient.java
+++ src/com/zen/http/TestPostByHttpClient.java
@@ -1,12 +1,6 @@
 package com.zen.http;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +9,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
